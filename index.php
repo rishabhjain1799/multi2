d54499e334543aa1bc4e078ddf967727e623e881
rishabhjain1799 <54940406+rishabhjain1799@users.noreply.github.com>
2020-08-28 13:13:32 +0530

    web

---
+++ index.php
@@ -0,0 +1,15 @@
+<body bgcolor = "aqua">
+<img src = "http://d1g96frk5ozyyv.cloudfront.net/me.jpg" height = "200" width = "200"></img>
+<pre>
+<?php
+  
+print "HIII CLIENT";
+
+
+print `ifconfig`;
+
+
+print `free -m`;
+
+?>
+</pre>
